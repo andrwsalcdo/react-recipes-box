0b112aedf88bbaa5c138003618ea56914e55f470
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-22 20:12:46 -0400

    App componenet layout. todo: seed data & local storage methods

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//child components 
+  // import Header from './components/Header/Header'; 
+  // import RecipeBoxContainer from './components/Containers/RecipeBoxContainer';
+  // import Footer from './components/Footer/Footer';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          {/*<Header />
+          <RecipeBoxContainer />
+          <Footer /> */}
       </div>
     );
   }
