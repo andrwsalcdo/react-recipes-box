d289f599e6f803f930c2fb92ed23652b42aeb097
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-02 21:01:21 -0400

    styling: button

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -9,25 +9,52 @@ const Button = styled.button`
     margin: 0; 
     outline: none;
     padding: 6px;
+    width: 25%; 
+ 
+    :active {
+        transform: translateY(5px); 
+    }
+
+    @media only screen and (max-width: 655px) {
+        width: 80%
+    }
 
     ${props => props.add_btn && css`
-    background: #F39C12;
-    color: white;
+        background: #F39C12;
+        color: white;
+        
+        @media only screen and (max-width: 655px) {
+            margin-right: 1em; 
+        }
     `}
     
     ${props => props.recipe_btn && css`
-    background: #03C9A9;
-    color: white;
+        background: #03C9A9;
+        color: white;
     `}
     
     ${props => props.edit_btn && css`
-    background: #F5D76E;
-    color: white;
+        background: #F5D76E;
+        color: white;
+        margin-right: 5%; 
+        width: 15%; 
+        
+        @media only screen and (max-width: 655px) {
+            width: 80%; 
+            margin: 0; 
+        }
     `}
 
     ${props => props.delete_btn && css`
-    background: salmon; 
-    color: white;
+        background: salmon; 
+        color: white;
+        margin-left: 5%; 
+        width: 15%; 
+        
+        @media only screen and (max-width: 655px) {
+            width: 80%; 
+            margin: 0; 
+        }
     `}
 
 `;
