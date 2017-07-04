3484b361e178a0b4f8defd23526e4027c4ba9894
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-04 16:16:50 -0400

    style: alert modal

--- src/Views/Alert/AlertItem.js
+++ src/Views/Alert/AlertItem.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 import Button from '../../Components/Button'; 
 
+
 const AlertItem = (props) => (
-    <Modal show={props.alertIsOpen} onHide={props.closeAlert}>
-        <p>Are you sure you want to delete this?</p>
-        <Button onClick={props.delete}>Yes</Button>
-        <Button onClick={props.closeAlert}>No</Button>
+    <Modal show={props.alertIsOpen} onHide={props.closeAlert} className="text-center">
+        <Modal.Body className="alert-modal">
+            <p>Are you sure you want to delete this?</p>
+            <Button yes_btn onClick={props.delete}>Yes</Button>
+            <Button no_btn onClick={props.closeAlert}>No</Button>            
+        </Modal.Body>
     </Modal>
 )
 
